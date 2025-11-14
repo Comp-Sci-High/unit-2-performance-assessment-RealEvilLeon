f50b88b2e58a5c3d14704fb89ac95cae03dc8fe0
FMxRadio <118459922+RealEvilLeon@users.noreply.github.com>
2025-11-14 20:18:31 +0000

    ThePush

--- chatgpt.js
+++ chatgpt.js
@@ -1,7 +1,34 @@
 const env = require('dotenv').config({ quiet: true });
+
 const prompt = require('prompt-sync')();
+
 const apiKey = process.env.apiKey1
 
 // run your code with node chatgpt.js
 // add your apiKey to .env from gChat
 // Follow the checklist on the Performance Assessment Google Doc
+
+let requestURL = "https://api.openai.com/v1/responses"
+
+async function chatgptBot(promptData){
+
+    let options = {
+        method: 'POST',
+        headers:{
+            Authorization:'Bearer ' +apiKey,
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(promptData)
+    }
+
+    // the actual calling
+    let response = await fetch(requestURL,options)
+    let actualData = await response.json()
+    console.log(actualData)
+    return actualData
+}
+
+chatgptBot({
+    model:"gpt-4.1",
+    input:"Tell me a three sentence bedtime story about a unicorn."
+})
