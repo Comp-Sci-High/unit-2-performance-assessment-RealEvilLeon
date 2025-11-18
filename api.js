6fcbd62fd37ee0046dfc01b4575a04005d738a67
FMxRadio <118459922+RealEvilLeon@users.noreply.github.com>
2025-11-18 19:17:55 +0000

    push

--- api.js
+++ api.js
@@ -1,7 +1,26 @@
 const env = require('dotenv').config({ quiet: true });
-const prompt = require('prompt-sync')();
-const apiKey = process.env.apiKey2
 
-// run your code with node api.js
-// add your apiKey to .env if your API needs one, otherwise ignore
+const prompt = require('prompt-sync')();
+
+const apiKey = process.env.apiKey1
+
+// run your code with node chatgpt.js
+// add your apiKey to .env from gChat
 // Follow the checklist on the Performance Assessment Google Doc
+
+let requestURL = "https://thesimpsonsapi.com/api"
+
+console.log("Welcome To The Simpson Generator!")
+console.log("Please Type In Your Choice")
+let choice = 
+
+async function SimpsonGenerator(promptData){
+// the actual calling
+    let response = await fetch(requestURL)
+    let actualData = await response.json()
+    console.log(actualData)
+    return actualData
+}
+
+
+
