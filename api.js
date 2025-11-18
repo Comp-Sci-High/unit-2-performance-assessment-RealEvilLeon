a5837f887df60bae611fb7bd745c16235c03542a
FMxRadio <118459922+RealEvilLeon@users.noreply.github.com>
2025-11-18 20:02:43 +0000

    TheFinalPush

--- api.js
+++ api.js
@@ -8,19 +8,36 @@ const apiKey = process.env.apiKey1
 // add your apiKey to .env from gChat
 // Follow the checklist on the Performance Assessment Google Doc
 
-let requestURL = "https://thesimpsonsapi.com/api"
+let baseURL = "https://thesimpsonsapi.com/api"
 
 console.log("Welcome To The Simpson Generator!")
 console.log("Please Type In Your Choice")
-let choice = 
+let choice = prompt("Characters, Episodes, or Locations? ").toLowerCase()
 
-async function SimpsonGenerator(promptData){
+let requestURL = baseURL + "/" + choice
+
+let choiceId = prompt("Enter a Number: ")
+let theRequestURL = requestURL + "/" + choiceId
+
+console.log("Generating...")
+
+
+
+async function simpsonGenerator(url){
 // the actual calling
-    let response = await fetch(requestURL)
+
+    let response = await fetch(url)
     let actualData = await response.json()
-    console.log(actualData)
+
+    if(choice === "Characters" || choice === "characters"){
+        let randPhrase = Math.floor(Math.random() * actualData.phrases.length)
+        console.log(actualData.phrases[randPhrase])
+    }
+    
+
+    console.log(actualData.name)
     return actualData
 }
 
-
+simpsonGenerator(theRequestURL)
 
